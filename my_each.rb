ad7082dd5cf148d923fb68a5dff6cbfd21a6c10e
vy vo <tuonggvy.vo@gmail.com>
2019-11-05 05:04:25 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,12 @@ def my_each(array) # put argument(s) here
   # code here
   x = 0
   while x < array.length
-    yield(array[x])
+    yield
     x = x + 1
   end
   array
 end
 
-
+my_each(array) do
+  array[x]
+end
