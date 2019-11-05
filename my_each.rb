2ae616fa8df1f9555352a400e63266cb773b6b38
vy vo <tuonggvy.vo@gmail.com>
2019-11-05 05:03:27 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  x = 0
+  while x < array.length
+    yield(array[x])
+    x = x + 1
+  end
+  array
 end
+
+
